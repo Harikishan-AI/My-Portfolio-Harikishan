af925f085269feff52b921f6099f827881a638a8
Harikishan Takoor <71831642+Harikishan-AI@users.noreply.github.com>
2022-02-09 12:11:13 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,31 @@
+// DOM elements
+const swapBtn = document.querySelector('#swap-btn')
+const container = document.querySelector('#container')
+const icons = document.querySelector('#icons')
+
+// get data stored in localStorage and set up app
+let dark = JSON.parse(localStorage.getItem('animated-icons-darkmode')) || false
+if (dark) {
+	container.classList.add('dark')
+	swapBtn.textContent = 'Switch to Light Mode'
+	icons.style.setProperty('--rotation', 180)
+}
+
+// Event listener on button
+swapBtn.addEventListener('click', () => {
+	container.classList.toggle('dark')
+	const rotation = parseInt(getComputedStyle(icons).getPropertyValue('--rotation'))
+	icons.style.setProperty('--rotation', rotation - 180)
+	dark = !dark
+	if (dark) {
+		swapBtn.textContent = 'Switch to Light Mode'
+	} else {
+		swapBtn.textContent = 'Switch to Dark Mode'
+	}
+	localStorage.setItem('animated-icons-darkmode', JSON.stringify(dark))
+})
+
+
 /*===== MENU SHOW =====*/ 
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
